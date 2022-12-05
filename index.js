7b88078501398fafea0bc5d6b34cd8cd96c8e1f3
Moussaoui Asmaa <76711091+moussaoui25@users.noreply.github.com>
2022-12-05 19:42:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const pg = require("pg");
 const cors = require("cors")
 const data = require('./Bureau_TOPO.json')
 const bodyParser = require("body-parser");
-require("dotenv").config();
 
 
 
@@ -19,7 +18,7 @@ const app = express()
 app.use(cors())
 const port = process.env.PORT || 5000
 const mydata = data.features
-var search_result = []
+
 
 
 
@@ -29,13 +28,13 @@ app.get('/', (req, res)=>{
 })
 
 app.get('/db', async (req, res)=>{
-  var data_res = await (await pool.query("SELECT id, name,  offre_ing, offre_tech, st_x(geom) as lng, st_y(geom) as lat FROM data")).rows
+  var data_res = await (await pool.query("SELECT id, name,  offre_ing, offre_tech FROM data")).rows
   res.json({data_res})
 })
 
 app.post('/add_all', (req, res)=>{
-    mydata.features.forEach((feature)=>{
-        var geom = `POINT(${feature.geometry.coordinates[0]} ${feature.geometry.coordinates[1]})`
+    mydata.forEach((feature)=>{
+        var geom = `POINT(${feature.geometry.coordinates[0]} ${feature.geometry.coordinates[1]});`
         var offre_ing = feature.properties.Offre_ing;
         var offre_tech = feature.properties.Offre_tech;
         var nom = feature.properties.Nom;
@@ -44,6 +43,5 @@ app.post('/add_all', (req, res)=>{
     })
     res.send('done')
 })
-
 
   app.listen(port, console.log('app running'));
